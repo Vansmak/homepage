416edc49937edbc3dd038153d3eae4eb279de572
Vansmak <i.am.vansmak@gmail.com>
2023-08-09 13:04:06 -0700

    Update widget.js

--- src/components/widgets/player/widget.js
+++ src/components/widgets/player/widget.js
@@ -4,7 +4,7 @@ export const api = "http://192.168.254.64:8123/api/";
 
 export const options = {
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI5Y2FkOTgxY2MxNjc0NWZjOGIyYThhMTViNzc4ZTg4MSIsImlhdCI6MTY3OTY4OTkyNCwiZXhwIjoxOTk1MDQ5OTI0fQ.V70tZDc1KjSqYC5GrUybKY9bAuI-kZbU8241xgBeqZ8`,
+    Authorization: `Bearer longlivedaccesstoken`,
     "Content-Type": "application/json",
   },
 };
